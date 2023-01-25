dda3e9a661a9ae3e008faedd6c6df8ff18ff1ad8
Shakeel Akthar <shakeelakhther@gmail.com>
2023-01-26 03:36:57 +0530

    updated

--- lib/Pages/Home.dart
+++ lib/Pages/Home.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
-import 'package:poll/Widgets/Question.dart';
+// import 'package:poll/Widgets/Question.dart';
 
 
 class HomePage extends StatefulWidget {
